26fb1c116335997a2eae9b8b81746843802571e8
Hussien Liban <mntstyle@gmail.com>
2015-10-02 22:53:10 +0300

    cssnext working

--- gulp/tasks/dev/css.js
+++ gulp/tasks/dev/css.js
@@ -1,13 +1,26 @@
-var gulp     = require('gulp'),
-    postCss  = require('gulp-postcss'),
-    cssnext  = require('gulp-cssnext'),
-    config   = require('../../config').css.dev;
+var gulp      = require('gulp'),
+    notify    = require("gulp-notify"),
+    postcss   = require('gulp-postcss'),
+    cssnext   = require('postcss-cssnext'),
+    message   = require('../../notifyMessages').css,
+    config    = require('../../config').css.dev;
 
 
 
 gulp.task('css:dev', function(){
-  console.log('processing css')
+ var processors = [
+        cssnext({
+            'browers':          ['last 2 version'],
+            'customProperties': true,
+            'colorFunction':    true,
+            'customSelectors':  true,
+            'sourcemap':        true,
+            'customProperties': true,
+            'compress':         false
+        })
+    ];
   return gulp.src(config.src)
-             .pipe(postCss([cssnext]))
+             .pipe(notify(message))
+             .pipe(postcss(processors))
              .pipe(gulp.dest(config.dest));
 });

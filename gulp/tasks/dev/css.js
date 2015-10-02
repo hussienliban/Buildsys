d8977cfcfc45f08094ee2a2289493c502e32cf09
Hussien Liban <mntstyle@gmail.com>
2015-10-02 23:01:52 +0300

    Moved options to config file

--- gulp/tasks/dev/css.js
+++ gulp/tasks/dev/css.js
@@ -1,23 +1,16 @@
-var gulp      = require('gulp'),
-    notify    = require("gulp-notify"),
-    postcss   = require('gulp-postcss'),
-    cssnext   = require('postcss-cssnext'),
-    message   = require('../../notifyMessages').css,
-    config    = require('../../config').css.dev;
+var gulp       = require('gulp'),
+    notify     = require("gulp-notify"),
+    postcss    = require('gulp-postcss'),
+    cssnext    = require('postcss-cssnext'),
+    message    = require('../../notifyMessages').css,
+    config     = require('../../config').css.dev,
+    psOption   = require('../../config').postcss;
 
 
 
 gulp.task('css:dev', function(){
  var processors = [
-        cssnext({
-            'browers':          ['last 2 version'],
-            'customProperties': true,
-            'colorFunction':    true,
-            'customSelectors':  true,
-            'sourcemap':        true,
-            'customProperties': true,
-            'compress':         false
-        })
+        cssnext(psOption.cssnext),
     ];
   return gulp.src(config.src)
              .pipe(notify(message))
